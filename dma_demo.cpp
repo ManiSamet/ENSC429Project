f2a3a6a379862e4b712f9e4d0a60179393315329
ManiSamet <manisamet4@gmail.com>
2025-08-04 13:01:46 -0700

    sine wave

--- dma_demo.cpp
+++ dma_demo.cpp
@@ -20,6 +20,7 @@
 #define SINE_WAVE_TABLE_LEN 2048
 #define SAMPLES_PER_BUFFER 1156 // Samples / channel
 
+void sine_wave();
 
 static const uint32_t PIN_DCDC_PSM_CTRL = 23;
 
@@ -38,7 +39,7 @@ static const uint32_t PIN_DCDC_PSM_CTRL = 23;
 #define OUTPUT_SLICE ONBOARD_LED_SLICE
 #define OUTPUT_CHANNEL ONBOARD_LED_CHANNEL
 
-#define ADC_SAMPLE_SIZE 2048
+#define ADC_SAMPLE_SIZE 64
 #define ADC_PIN 27
 #define ADC_CHANNEL 1
 
@@ -52,7 +53,7 @@ volatile bool buffer_full[2] = {false, false};
 volatile bool switch_buffers = true;  //adc should startfist with this
 
 audio_buffer_pool_t *ap;
-static bool decode_flg = false;
+volatile bool decode_flg = false;
 static constexpr int32_t DAC_ZERO = 1;
 
 #define audio_pio __CONCAT(pio, PICO_AUDIO_I2S_PIO)
@@ -87,21 +88,7 @@ uint vol = 20;
 
 void adc_callback();
 void decode();
-extern "C" {
-// callback from:
-//   void __isr __time_critical_func(audio_i2s_dma_irq_handler)()
-//   defined at my_pico_audio_i2s/audio_i2s.c
-//   where i2s_callback_func() is declared with __attribute__((weak))
-void i2s_callback_func()
-{
-    if (decode_flg && adc_flag) {
-		adc_flag = false;
-		gpio_xor_mask(1 << EXTERNAL_LED);
-		// gpio_put(ONBOARD_LED, 0);
-        decode();
-    }
-}
-}
+
  
 void init_adc(){
 
@@ -112,14 +99,14 @@ void init_adc(){
 
 	adc_fifo_setup( 
 		true, // enable FIFO
-		false, // disable DMA
-		2049, // trigger irq on 8 samples
+		false, // disables DMA
+		4, // trigger irq on 8 samples
 		false, // disable error sample flag
-		false // doesnt shift down to 8 bytes
+		false // Shift FIFO contents to be one byte in size (for byte DMA) - enables DMA to byte buffers
 	);
 
-	// adc_set_clkdiv((48e6 / 44100) - 1);  
-	adc_set_clkdiv(15e6);
+	// adc_set_clkdiv((48e6 / 16000) - 1);  //48 MHz/ (div + 1) = adc frequency
+	adc_set_clkdiv(48000.0f);
 
 	// enable interrupt
 
@@ -202,6 +189,7 @@ static inline uint32_t _millis(void)
 	return to_ms_since_boot(get_absolute_time());
 }
 
+
 void i2s_audio_deinit()
 {
     decode_flg = false;
@@ -276,8 +264,8 @@ int main() {
 
     stdio_init_all();
 	init_led();
-    init_adc();
-	adc_run(true);
+    // init_adc();
+	// adc_run(true); 
     // Set PLL_USB 96MHz
     pll_init(pll_usb, 1, 1536 * MHZ, 4, 4);
     clock_configure(clk_usb,
@@ -291,137 +279,144 @@ int main() {
         CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_CLKSRC_PLL_USB,
         96 * MHZ,
         96 * MHZ);
-    // // CLK peri is clocked from clk_sys so need to change clk_peri's freq
-    // clock_configure(clk_peri,
-    //     0,
-    //     CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS,
-    //     96 * MHZ,
-    //     96 * MHZ);
-    // Reinit uart now that clk_peri has changed
+    // CLK peri is clocked from clk_sys so need to change clk_peri's freq
+    clock_configure(clk_peri,
+        0,
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS,
+        96 * MHZ,
+        96 * MHZ);
+    // // Reinit uart now that clk_peri has changed
     stdio_init_all();
 
-    // DCDC PSM control
-    // 0: PFM mode (best efficiency)
-    // 1: PWM mode (improved ripple)
-    // gpio_init(PIN_DCDC_PSM_CTRL);
-    // gpio_set_dir(PIN_DCDC_PSM_CTRL, GPIO_OUT);
-    // gpio_put(PIN_DCDC_PSM_CTRL, 1); // PWM mode for less Audio noise
+    gpio_init(PIN_DCDC_PSM_CTRL);
+    gpio_set_dir(PIN_DCDC_PSM_CTRL, GPIO_OUT);
+    gpio_put(PIN_DCDC_PSM_CTRL, 1); // PWM mode for less Audio noise
 
-    // for (int i = 0; i < SINE_WAVE_TABLE_LEN; i++) {
-    //     sine_wave_table[i] = 32767 * cosf(i * 2 * (float) (M_PI / SINE_WAVE_TABLE_LEN));
-    // }
+    for (int i = 0; i < SINE_WAVE_TABLE_LEN; i++) {
+        sine_wave_table[i] = 32767 * cosf(i * 2 * (float) (M_PI / SINE_WAVE_TABLE_LEN));
+    }
 
     ap = i2s_audio_init(41000); //TODO: Possible change to fix
 	// gpio_xor_mask(1 << ONBOARD_LED);
     while (true) {
 		// printf("TEST\n");
+        // sine_wave();
+		// if(decode_flag){
+		// 	// adc_flag = false;
+		// gpio_xor_mask(1 << ONBOARD_LED);
+		// // sine_wave();
+		// decode();
+		// }
     }
     return 0;
 }
-/*zeros*/
-// void decode(){
-//  audio_buffer_t *buffer = take_audio_buffer(ap, false);
-//     if (buffer == NULL) return;
 
-//     int16_t *samples = (int16_t *) buffer->buffer->bytes;
-//     for (uint i = 0; i < buffer->max_sample_count; i++) {
-//         samples[i*2 + 0] = 0;  // left
-//         samples[i*2 + 1] = 0;  // right
-//     }
-//     buffer->sample_count = buffer->max_sample_count;
-//     give_audio_buffer(ap, buffer);
-// }
-void decode(){
-	   audio_buffer_t *buffer = take_audio_buffer(ap, false);
-    if (buffer == NULL) return;
-		// gpio_xor_mask(1 << ONBOARD_LED);
+void sine_wave(){
+    audio_buffer_t *buffer = take_audio_buffer(ap, false);
+    if (buffer == NULL) { return; }
+    int32_t *samples = (int32_t *) buffer->buffer->bytes;
+    for (uint i = 0; i < buffer->max_sample_count; i++) {
 
-    int16_t *samples = (int16_t *) buffer->buffer->bytes;
+		int32_t value0 = (vol * sine_wave_table[pos0 >> 16u]) << 8u;
+        int32_t value1 = (vol * sine_wave_table[pos1 >> 16u]) << 8u;
+        // use 32bit full scale
+        samples[i*2+0] = value0 + (value0 >> 16u);  // L
+        samples[i*2+1] = value1 + (value1 >> 16u);  // R
+        pos0 += step0;
+        pos1 += step1;
+        if (pos0 >= pos_max) pos0 -= pos_max;
+        if (pos1 >= pos_max) pos1 -= pos_max;
 
-    int buf_to_read = -1;
-    if (buffer_full[0]) buf_to_read = 0;
-    else if (buffer_full[1]) buf_to_read = 1;
+		// int32_t centered = ((int32_t)(sample_buffer[buffer_number][i])-2048) << 20;
+		// int32_t centered = *(int32_t*)&sample_buffer[buffer_number][i];
 
-    if (buf_to_read == -1) {
-		//silent buffer
-        for (uint i = 0; i < buffer->max_sample_count; i++) {
-            samples[i*2 + 0] = 0;
-            samples[i*2 + 1] = 0;
-        }
-    } else {
+        // int32_t value0 = (centered); //<< 8u;
+        // int32_t value1 = (centered); //<< 8u;
 
-        for (uint i = 0; i < buffer->max_sample_count; i++) {
-            int16_t val = ((int32_t)sample_buffer[buf_to_read][i] - 2048) << 4;
-            samples[i*2 + 0] = val;
-            samples[i*2 + 1] = val;
-        }
-        buffer_full[buf_to_read] = false;
+        // // use 32bit full scale
+        // samples[i*2+0] = centered; //+ (value0 >> 16u);  // L
+        // samples[i*2+1] = centered; //+ (value1 >> 16u);  // R
+		// int32_t centered = ((int32_t)sample_buffer[buffer_number][i] - 2048) << 20;
+        // samples[i*2 + 0] = centered; // Left
+        // samples[i*2 + 1] = centered; // Right
     }
-
     buffer->sample_count = buffer->max_sample_count;
     give_audio_buffer(ap, buffer);
+
 }
+// void decode(){
+// 	   audio_buffer_t *buffer = take_audio_buffer(ap, false);
+//     if (buffer == NULL) return;
+// 		// gpio_xor_mask(1 << ONBOARD_LED);
 
+//     int16_t *samples = (int16_t *) buffer->buffer->bytes;
 
-// void decode()
-// {
-// 	// Ping pong buffering; grab from the one that isn't being touched by producer
-// 	int buffer_number = buffer;
-// 	buffer = !buffer; // signal to switch buffers in the adc
-// 	switch_buffers = true;
+//     int buf_to_read = -1;
+//     if (buffer_full[0]) buf_to_read = 0;
+//     else if (buffer_full[1]) buf_to_read = 1;
 
-//     audio_buffer_t *buffer = take_audio_buffer(ap, false);
-//     if (buffer == NULL) { return; }
-//     int32_t *samples = (int32_t *) buffer->buffer->bytes;
-//     for (uint i = 0; i < buffer->max_sample_count; i++) {
+//     if (buf_to_read == -1) {
+// 		//silent buffer
+//         for (uint i = 0; i < buffer->max_sample_count; i++) {
+//             samples[i*2 + 0] = 0;
+//             samples[i*2 + 1] = 0;
+//         }
+//     } else {
 
-// 		// int32_t value0 = (vol * sine_wave_table[pos0 >> 16u]) << 8u;
-//         // int32_t value1 = (vol * sine_wave_table[pos1 >> 16u]) << 8u;
-//         // // use 32bit full scale
-//         // samples[i*2+0] = value0 + (value0 >> 16u);  // L
-//         // samples[i*2+1] = value1 + (value1 >> 16u);  // R
-//         // pos0 += step0;
-//         // pos1 += step1;
-//         // if (pos0 >= pos_max) pos0 -= pos_max;
-//         // if (pos1 >= pos_max) pos1 -= pos_max;
-
-// 		// int32_t centered = ((int32_t)(sample_buffer[buffer_number][i])-2048) << 20;
-// 		// int32_t centered = *(int32_t*)&sample_buffer[buffer_number][i];
-
-//         // int32_t value0 = (centered); //<< 8u;
-//         // int32_t value1 = (centered); //<< 8u;
-
-//         // // use 32bit full scale
-//         // samples[i*2+0] = centered; //+ (value0 >> 16u);  // L
-//         // samples[i*2+1] = centered; //+ (value1 >> 16u);  // R
-// 		int16_t raw_data = adc_read();
-//         int32_t centered = ((int32_t)raw_data - 2048) << 20;
-//         samples[i*2 + 0] = centered; // Left
-//         samples[i*2 + 1] = centered; // Right
-// 		// int32_t centered = ((int32_t)sample_buffer[buffer_number][i] - 2048) << 20;
-//         // samples[i*2 + 0] = centered; // Left
-//         // samples[i*2 + 1] = centered; // Right
+//         for (uint i = 0; i < buffer->max_sample_count; i++) {
+//             int16_t val = ((int32_t)sample_buffer[buf_to_read][i] - 2048) << 4;
+//             samples[i*2 + 0] = val;
+//             samples[i*2 + 1] = val;
+//         }
+//         buffer_full[buf_to_read] = false;
 //     }
+
 //     buffer->sample_count = buffer->max_sample_count;
 //     give_audio_buffer(ap, buffer);
-//     return;
 // }
+
+
+void decode()
+{
+    audio_buffer_t *buffer = take_audio_buffer(ap, false);
+    if (buffer == NULL) { return; }
+    int32_t *samples = (int32_t *) buffer->buffer->bytes;
+    for (uint i = 0; i < buffer->max_sample_count; i++) {
+        int32_t value0 = (vol * sine_wave_table[pos0 >> 16u]) << 8u;
+        int32_t value1 = (vol * sine_wave_table[pos1 >> 16u]) << 8u;
+        // use 32bit full scale
+        samples[i*2+0] = value0 + (value0 >> 16u);  // L
+        samples[i*2+1] = value1 + (value1 >> 16u);  // R
+        pos0 += step0;
+        pos1 += step1;
+        if (pos0 >= pos_max) pos0 -= pos_max;
+        if (pos1 >= pos_max) pos1 -= pos_max;
+    }
+    buffer->sample_count = buffer->max_sample_count;
+    give_audio_buffer(ap, buffer);
+    return;
+}
+
 void adc_callback(){
 
     // while (!adc_fifo_is_empty()) { 
-
+	for(int i = index; i < index+4; i++){
         uint16_t raw = adc_fifo_get();
-		adc_fifo_drain(); // resets event
-        sample_buffer[buffer][index++] = raw;
-		printf("%d\n", raw);
+        index = i;
+        sample_buffer[buffer][index] = raw;
         if (index >= ADC_SAMPLE_SIZE) {
             buffer_full[buffer] = true;
             buffer = !buffer;
             index = 0;
             gpio_xor_mask(1 << ONBOARD_LED);
-			// gpio_put(ONBOARD_LED, 1);
-			adc_flag = true;
+            // gpio_put(ONBOARD_LED, 1);
+            adc_flag = true;
+            break;
         }
+		// printf("%d\n", raw);
+	}
+
+	adc_fifo_drain(); // resets event
     // }
     irq_clear(ADC_IRQ_FIFO);
 
@@ -440,3 +435,18 @@ void adc_callback(){
 	// irq_clear(ADC_IRQ_FIFO);
 }
 
+extern "C" {
+// callback from:
+//   void __isr __time_critical_func(audio_i2s_dma_irq_handler)()
+//   defined at my_pico_audio_i2s/audio_i2s.c
+//   where i2s_callback_func() is declared with __attribute__((weak))
+void i2s_callback_func()
+{
+    gpio_xor_mask(1 << EXTERNAL_LED);
+    if (decode_flg /*&& adc_flag*/) {
+		// adc_flag = false;
+		// gpio_put(ONBOARD_LED, 0);
+        decode();
+    }
+}
+}
